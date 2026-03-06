98fdb1b4f6537679257832766120d6c2615fa49a
mhmdodommg <totomnsor090@gmail.com>
2026-03-06 11:31:41 +0000

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,10 @@ export default function RootLayout({
           rel="stylesheet" 
           href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" 
         />
+        
+       <link rel="stylesheet" href="https://sudanlibrary.kesug.com/https/cnneiniaye398486/njenducu8286863-26745959/main-style.css" />
+       <link rel="stylesheet" href="https://sudanlibrary.kesug.com/https/cnneiniaye398486/njenducu8286863-26745959/navbar.css" />
+
       </head>
       <body className={tajawal.className}>{children}</body>
     </html>
